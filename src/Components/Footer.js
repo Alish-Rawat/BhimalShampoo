9c7dba00189338026f430c52ebbade5b57b6953c
Alish <rawatalish4@gmail.com>
2023-11-25 11:55:53 +0530

    updates 3

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="w-full sm:flex justify-evenly items-center text-md sm:text-lg">
         <div className="sm:m-2 m-6 p-2 sm:flex justify-center ">
           <p>
-            Contacts : 7251958143 , 9012345287 <br />
+            Contacts : 7251958143 , 6396449828 <br />
             Email :
             <span className="  tracking-wide">
               &nbsp; bishtenterprises2022@gmail.com
@@ -18,8 +18,8 @@ const Footer = () => {
           {/* <p>Contacts : </p> 7251958143 , <span> 9012345287</span> */}
         </div>
         <p className=" m-2 pl-6  flex justify-center sm:block">
-          Address : near Abhimanyu Cricket Academy P.O. Sinola, Jamoliwala,
-          <br /> Bhagwant Pur, Guniyal Gaon, Uttarakhand 248001, India
+          Address : Vill- Chamali , Post- Satpali , Patty-Ringwar Syun
+          <br /> Distt - Pauri Garhwal - Uttarakhand ( 246167 ) - India
         </p>
       </div>
     </div>
